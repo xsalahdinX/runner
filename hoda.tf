3c302b5ce3c9dae526c22f2b46385bac62c66c97
xsalahdinX <102380926+xsalahdinX@users.noreply.github.com>
2022-04-08 00:39:01 +0200

    Update hoda.tf

--- hoda.tf
+++ hoda.tf
@@ -217,3 +217,15 @@ default_action {
   }
 
 }
+
+
+terraform {
+  backend "s3" {
+    bucket         = "tharwat-bucket"
+    key            = "Group1/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+
+
